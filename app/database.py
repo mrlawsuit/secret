23320d4a34b77584487579ade015123dad45f6fd
mrlawsuit <opronin1992@gmail.com>
2024-11-23 21:10:46 +0200

    add db init

--- app/database.py
+++ app/database.py
@@ -3,7 +3,20 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from config import DB_URL
+from models import Base
 
 engine = create_async_engine(DB_URL)
 
 async_session = async_sessionmaker(engine)
+
+def create_tables(sync_engine):
+    Base.metadata.create_all(bind=sync_engine)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(create_tables)
+
+
+async def dispose_engine():
+    await engine.dispose()
