86bdff7c966490f6f23a05fdfa43bba136301e2b
mrlawsuit <opronin1992@gmail.com>
2024-11-23 21:11:25 +0200

    add generate endpoint

--- app/main.py
+++ app/main.py
@@ -1,16 +1,36 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, HTTPException, Depends
 
+from app.repositories import secret_repository as secret_db
+import schemas as shm
+import models
+from database import init_db, dispose_engine
 
-import schemas
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    print('Database is successfully initiated')
+
+    yield
+
+    await dispose_engine()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
-@app.post('/generate', response_model=schemas.SecretKeyResponse)
+@app.post('/generate', response_model=shm.SecretKeyResponse)
 async def generate_secret(
-    secret: schemas.SecretCreate
+    secret: shm.SecretCreate
 ):
     '''Generate secret key for a one-time secret.'''
-    pass
+    secret_key = await secret_db.create_secret(secret)
+    return {'secret_key': secret_key}
 
+
+@app.get('/secrets/{secret_key}')
+async def get_secret(secret_key: str, passphrase: str):
+    '''Recieve and decrypt a one-time secret usinfg secret key and passphrase'''
+    pass
